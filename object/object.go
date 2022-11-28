c6830b5c7fa830591fed4513c6a67450111017a4
Carlos Hernandez <xyc.hernandez@gmail.com>
2022-11-27 21:21:31 -0600

    Evaluation
    3.7 Return Statements.

--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -23,6 +24,10 @@ type Null struct {
 
 type Boolean struct {
 	Value bool
+}
+
+type ReturnValue struct {
+	Value Object
 }
 
 func (i *Integer) Inspect() string {
@@ -47,4 +52,12 @@ func (n *Null) Inspect() string {
 
 func (n *Null) Type() ObjectType {
 	return NULL_OBJ
+}
+
+func (rv *ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
+}
+
+func (rv *ReturnValue) Type() ObjectType {
+	return RETURN_VALUE_OBJ
 }
